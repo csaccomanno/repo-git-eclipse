b2c2023996d2ba32c1c06a9076eb15fa6b40ba9f
csaccomanno <saccomanno@gmail.com>
2022-04-06 02:30:25 -0300

    Commit spring-ws-json v0.1

---
+++ spring-ws-json/src/main/java/spring/ws/json/App.java
@@ -0,0 +1,13 @@
+package spring.ws.json;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
